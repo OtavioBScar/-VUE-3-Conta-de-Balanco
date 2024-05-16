d94369827f594a0e55fd0a3543ae7705ceb02906
OtavioBScar <otavio.scarpelline@gmail.com>
2024-05-16 16:44:05 -0300

    chore: DefineProps para puxar lista de transações

--- src/components/ListaTransacoes.vue
+++ src/components/ListaTransacoes.vue
@@ -1,17 +1,18 @@
 <script setup>
-    const transacoes = [
-        { id: 1, texto: "Flores", quantidade: -23.99 },
-        { id: 2, texto: "Salario", quantidade: 1442.87 },
-        { id: 3, texto: "Caneca", quantidade: -14.55 },
-    ];
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transacoes: {
+        type: Array,
+        required: true,
+    },
+});
 </script>
 
 <template>
     <h3>Histórico</h3>
     <ul id="lista" class="lista">
-        <li v-for="transacao in transacoes" 
-        :key="transacao.id" 
-        :class="transacao.quantidade < 0 ? 'minus' : 'plus'">
+        <li v-for="transacao in transacoes" :key="transacao.id" :class="transacao.quantidade < 0 ? 'minus' : 'plus'">
             {{ transacao.texto }} <span>{{ transacao.quantidade }}</span>
             <button class='delete-btn'>x</button>
         </li>
