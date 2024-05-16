7adc2c7c9f3a9152d28877a018e8a2030959d9fa
OtavioBScar <otavio.scarpelline@gmail.com>
2024-05-16 16:43:10 -0300

    chore: definindo setup e transformando transações em props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
-<script>
-  import Balanco from './components/Balanco.vue';
-  import Cabecalho from './components/Cabecalho.vue';
-  import DespesasRenda from './components/DespesasRenda.vue'
-  import ListaTransacoes from './components/ListaTransacoes.vue';
-  import AdicionarTransacao from './components/AdicionarTransacao.vue';
+<script setup>
+import Balanco from './components/Balanco.vue';
+import Cabecalho from './components/Cabecalho.vue';
+import DespesasRenda from './components/DespesasRenda.vue'
+import ListaTransacoes from './components/ListaTransacoes.vue';
+import AdicionarTransacao from './components/AdicionarTransacao.vue';
+import { ref } from 'vue'//Função para deixar a lista reativa
 
-  export default {
-    components: {
-      Cabecalho,
-      Balanco,
-      DespesasRenda,
-      ListaTransacoes,
-      AdicionarTransacao,
-    },
-  }
+const transacoes = ref([
+  { id: 1, texto: "Flores", quantidade: -23.99 },
+  { id: 2, texto: "Salario", quantidade: 1442.87 },
+  { id: 3, texto: "Caneca", quantidade: -14.55 },
+]);
 </script>
 
 <template>
@@ -21,8 +18,7 @@
   <div class="container">
     <Balanco />
     <DespesasRenda />
-    <ListaTransacoes />
+    <ListaTransacoes :transacoes="transacoes" />
     <AdicionarTransacao />
   </div>
 </template>
-
